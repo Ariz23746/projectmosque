68390e15704db732aa751fca5a433ad51bd0d95c
MOHAMMAD ARIZ KHAN <ariz@MOHAMMADs-MacBook-Air.local>
2019-08-25 23:50:52 +0530

    improved search,signin,signup

--- mosqueaccount/views.py
+++ mosqueaccount/views.py
@@ -9,30 +9,36 @@ from django.contrib import auth
 
 def signup(request):
 	if request.method == 'POST':
+		if request.POST['username'] and request.POST['password'] and request.POST['confirmpassword']:
+			if request.POST['password'] == request.POST['confirmpassword']:
+				try:
+					user = User.objects.get(username=request.POST['username'])
+					return render(request, 'mosqueaccount/signup.html', {'error':'Username has already been taken'})
+				except User.DoesNotExist:
+					User.objects.create_user(request.POST['username'], password=request.POST['password'])
+					auth.login(request,user)
+					return redirect('home')
 
-		if request.POST['password'] == request.POST['confirmpassword']:
-			try:
-				user = User.objects.get(username=request.POST['username'])
-				return render(request, 'mosqueaccount/signup.html', {'error':'Username has already been taken'})
-			except User.DoesNotExist:
-				User.objects.create_user(request.POST['username'], password=request.POST['password'])
-				auth.login(request,user)
-				return redirect('home')
-
+			else:
+				return render(request, 'mosqueaccount/signup.html',{'error':'password must match'})
 		else:
-			return render(request, 'mosqueaccount/signup.html',{'error':'password must match'})
+			return render(request, 'mosqueaccount/signup.html',{'error':'Fill all the fields'})
+
 	else:
 		
 		return render(request, 'mosqueaccount/signup.html')
 
 def signin(request):
 	if request.method == 'POST':
-		user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
-		if user is not None:
-			auth.login(request,user)
-			return redirect('home')
+		if request.POST['username'] and request.POST['password']:
+			user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+			if user is not None:
+				auth.login(request,user)
+				return redirect('home')
+			else:
+				return render(request ,'mosqueaccount/signin.html',{'error':'your username or password is incorrect'})
 		else:
-			return render(request ,'mosqueaccount/signin.html',{'error':'your username or password is wrong'})
+			return render(request,'mosqueaccount/signin.html',{'error':'All fields must be filled'})		
 	else:
 		return render(request,'mosqueaccount/signin.html')
 
